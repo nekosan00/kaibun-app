239f3b8c70793da72203285ef3e81f7e4bce79ea
nekoP <onasu_2@icloud.com>
2021-03-10 13:48:59 +0900

    kaibun

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <HelloWorld msg="回文作成">
+    <kaibun msg="回文作成">
   </div>
 </template>
 
 <script>
-import Helloworld from '@/components/kaibun.vue'
+import kaibun from '@/components/kaibun.vue'
 export default {
   name: 'App',
   components: {
